60c739b3f13dfcf6b5b8f891b9f513f7c01bd225
donglua <foolfalcon@hotmail.com>
2016-12-22 20:49:09 +0800

    rebuilding site 2016年12月22日 星期四 20时49分09秒 CST

--- deploy.sh
+++ deploy.sh
@@ -16,5 +16,5 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-#git push origin gh-pages
-git push origin `git subtree push --prefix=public git@github.com:donglua/donglua.github.io.git master`:master --force
+git push origin gh-pages
+git subtree push --prefix=public git@github.com:donglua/donglua.github.io.git master
